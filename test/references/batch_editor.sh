93a139a2a8172fd9d862abde5aeeaac5490d6682
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-09-25 16:08:52 +0200

    simplified batch_editor

--- test/references/batch_editor.sh
+++ test/references/batch_editor.sh
@@ -6,9 +6,10 @@ function batch_editor.sh {
   echo "Usage: ln -s $0 batch_editor_X; ./batch_editor_X file_to_edit"
 }
 
-function batch_editor_1 {
+function bundle_edit {
   sed -i "s/# ADD_NEW           SRC+BIN  OF 0ad / ADD_NEW           SRC+BIN  OF 0ad /" $@
 }
 
 cmd=$(basename $0)
+cmd=${cmd/batch_editor_/}
 ${cmd} $@
