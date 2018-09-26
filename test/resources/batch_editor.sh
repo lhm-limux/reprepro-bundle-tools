4b849d554c69076a9f0fa288bab4d539eef7bc3d
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-09-26 17:04:43 +0200

    added testcase bundle_05_meta

--- test/resources/batch_editor.sh
+++ test/resources/batch_editor.sh
@@ -10,6 +10,14 @@ function bundle_03_edit {
   sed -i "s/# ADD_NEW           SRC+BIN  OF 0ad / ADD_NEW           SRC+BIN  OF 0ad /" "$1"
 }
 
+function bundle_05_meta {
+  cat "$1" >repo/editor.in
+  sed -i "s/.Subject./This is my best test bundle/" "$1"
+  cat "$1" >repo/editor.out
+}
+
 cmd=$(basename $0)
 cmd=${cmd/batch_editor_/}
+
+echo "Calling batch-editor $cmd"
 ${cmd} "$@"
