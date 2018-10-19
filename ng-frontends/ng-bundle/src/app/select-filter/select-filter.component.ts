a53561d5b2e787138e37697c8427438933b3d9e9
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-10-20 01:45:26 +0200

    layout improvements and shorter logic

--- ng-frontends/ng-bundle/src/app/select-filter/select-filter.component.ts
+++ ng-frontends/ng-bundle/src/app/select-filter/select-filter.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-  selector: 'select-filter',
-  templateUrl: './select-filter.component.html',
+  selector: "select-filter",
+  templateUrl: "./select-filter.component.html",
   styles: []
 })
 export class SelectFilterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  @Input() field: string;
-  @Input() values: string[];
-  @Input() selected: Set<string> = new Set();
-  @Output() selectedChange = new EventEmitter<Set<string>>();
+  @Input()
+  field: string;
+  @Input()
+  values: string[];
+  @Input()
+  selected: Set<string> = new Set();
+  @Output()
+  selectedChange = new EventEmitter<Set<string>>();
 
   ngOnInit() {
-    this.values.forEach((v) => this.select(v));
+    this.values.forEach(v => this.selected.add(v));
   }
 
-  select(value) {
-      this.selected.add(value);
-      this.selectedChange.next(this.selected);
-  }
-
-  deselect(value) {
+  toggle(value) {
+    if (this.selected.has(value)) {
       this.selected.delete(value);
-      this.selectedChange.next(this.selected);
+    } else {
+      this.selected.add(value);
+    }
+    this.selectedChange.next(this.selected);
   }
-
 }
