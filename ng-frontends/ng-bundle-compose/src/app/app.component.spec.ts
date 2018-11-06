a00725f675c9e85894d96d506a9390c3a9a8539d
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-11-06 23:11:45 +0100

    using the shared lib with ng-bundle-compose

--- ng-frontends/ng-bundle-compose/src/app/app.component.spec.ts
+++ ng-frontends/ng-bundle-compose/src/app/app.component.spec.ts
@@ -1,31 +1,38 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { BundleListComponent } from "./bundle-list/bundle-list.component";
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { SelectFilterComponent } from "./select-filter/select-filter.component";
+import { BundleListService } from "./bundle-list/bundle-list.service";
+import { MockBundleListService } from "./test/mock-bundle-list-service.class";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, BundleListComponent, SelectFilterComponent],
+      providers: [
+        { provide: BundleListService, useClass: MockBundleListService }
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-bundle-compose'`, () => {
+  it(`should have as title 'Bundle-Compose-Tool'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-bundle-compose');
+    expect(app.title).toEqual("ng-bundle-compose");
   });
 
-  it('should render title in a h1 tag', () => {
+  it("should render title in a h1 tag", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-bundle-compose!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Bundle-Compose-Tool"
+    );
   });
 });
--- ng-frontends/ng-bundle-compose/src/app/app.component.ts
+++ ng-frontends/ng-bundle-compose/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from "@angular/core";
+import { BackendRegisterService } from "shared";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'ng-bundle-compose';
+export class AppComponent implements OnInit, OnDestroy {
+  title = "ng-bundle-compose";
+  hlB = false;
+
+  constructor(private backend: BackendRegisterService) {}
+
+  ngOnInit(): void {
+    this.backend.registerOnBackend();
+  }
+
+  ngOnDestroy(): void {
+    this.backend.unregisterFromBackend();
+  }
+
+  @HostListener("window:beforeunload", ["$event"])
+  private _storeSettings($event: any = null): void {
+    this.backend.unregisterFromBackend();
+  }
 }
