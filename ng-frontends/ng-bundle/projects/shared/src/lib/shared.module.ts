22b41c98fd0a42442ba898cc4d39e099e48ece12
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-11-06 17:43:02 +0100

    created shared library

---
+++ ng-frontends/ng-bundle/projects/shared/src/lib/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { SharedComponent } from './shared.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [SharedComponent],
+  exports: [SharedComponent]
+})
+export class SharedModule { }
