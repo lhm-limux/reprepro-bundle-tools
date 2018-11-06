3494a56696b97db3d7bf4d3604312110761a94ae
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-11-06 17:56:43 +0100

    adjusted public_api of lib shared and removed unused boilerplate code

--- ng-frontends/ng-bundle/projects/shared/src/public_api.ts
+++ ng-frontends/ng-bundle/projects/shared/src/public_api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of shared
  */
 
-export * from './lib/shared.service';
-export * from './lib/shared.component';
-export * from './lib/shared.module';
+export * from './lib/interfaces';
+export * from './lib/backend-register.service';
+export * from './lib/config.service';
