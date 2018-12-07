72a85dc0940c23289caffeb9357634c54b61ca55
Christoph Lutz <christoph.lutz@interface-ag.de>
2018-12-07 15:13:53 +0100

    added license header to ng-frontends code

--- ng-frontends/ng-bundle-libs/shared/src/lib/interfaces.ts
+++ ng-frontends/ng-bundle-libs/shared/src/lib/interfaces.ts
@@ -1,3 +1,21 @@
+/***********************************************************************
+* Copyright (c) 2018 Landeshauptstadt München
+*           (c) 2018 Christoph Lutz (InterFace AG)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the European Union Public Licence (EUPL),
+* version 1.1 (or any later version).
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* European Union Public Licence for more details.
+*
+* You should have received a copy of the European Union Public Licence
+* along with this program. If not, see
+* https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12
+***********************************************************************/
+
 export interface Bundle {
   name: string;
   distribution: string;
