58a050a2c114ae6f43b35478a8b22403d720bf4d
Christoph Lutz <christoph.lutz@interface-ag.de>
2019-01-16 16:10:53 +0100

    backend: cleaned up unused required-auth lines

--- reprepro_bundle_compose/app_server.py
+++ reprepro_bundle_compose/app_server.py
@@ -54,13 +54,9 @@ async def handle_required_auth(request):
         for authRef in req['refs']:
             if common_app_server.is_valid_authRef(authRef):
                 availableRefs.add(authRef['authId'])
-        if "publishChanges" == req['actionId']:
-            '''if not "ldap" in availableRefs:
-                res.append(common_interfaces.AuthType("ldap", "Required to publish changes into the shared GIT-Repository"))'''
-        elif "bundleSync" == req['actionId']:
+        if "bundleSync" == req['actionId']:
             if not "ldap" in availableRefs:
                 res.append(common_interfaces.AuthType("ldap", "Required for the synchronization with the Ticket system. Leave empty to skip trac-synchronisation."))
-                # res.append(common_interfaces.AuthType("ldap", "Required for the shared GIT-Repository recieve the latest status"))
             # if not "ldapAdmin" in availableRefs:
             #    res.append(common_interfaces.AuthType("ldapAdmin", "Required to create FAI-Classes for new bundles"))
         return web.json_response(res)
