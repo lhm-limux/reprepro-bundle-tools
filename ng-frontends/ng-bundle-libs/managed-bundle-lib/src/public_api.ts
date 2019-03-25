15f7422a29a96b7a7da9d609139e7f4166c17e19
Christoph Lutz <christoph.lutz@interface-ag.de>
2019-03-25 13:09:32 +0100

    Revert "ng generate library managed-bundle-lib; make clean install"
    This reverts commit 306bfc2c905a0d054e8b8ab6e146c2f26af652ea.

--- ng-frontends/ng-bundle-libs/managed-bundle-lib/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of managed-bundle-lib
- */
-
-export * from './lib/managed-bundle-lib.service';
-export * from './lib/managed-bundle-lib.component';
-export * from './lib/managed-bundle-lib.module';
