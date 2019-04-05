6924f73d4ea94ff5783f7169fbb003c907768a4e
Christoph Lutz <christoph.lutz@interface-ag.de>
2019-04-05 16:35:26 +0200

    ng-bundle-compose-viewer: using new shared select-filter now

--- ng-frontends/ng-bundle-compose-viewer/src/app/app.module.ts
+++ ng-frontends/ng-bundle-compose-viewer/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
+import { SharedModule } from "shared";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { BundleInfoCardComponent } from "./bundle-info-card/bundle-info-card.component";
-import { SelectFilterComponent } from './select-filter/select-filter.component';
 
 @NgModule({
-  declarations: [AppComponent, BundleInfoCardComponent, SelectFilterComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, BundleInfoCardComponent],
+  imports: [BrowserModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
