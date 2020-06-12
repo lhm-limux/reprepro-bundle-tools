0163d287c417506698a7281c91e8691dbca0797f
Daniel Bartlau <daniel.bartlau@muenchen.de>
2020-06-12 16:55:07 +0200

    Added configuration for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; -*- mode: emacs-lisp; fill-column: 79; -*-
+;;
+;;
+
+((python-mode . (python-indent-offset . 4))
+ (sh-mode . ((sh-basic-offset . 4)))
+ (nil . ((indent-tabs-mode . nil)
+	 (tab-width . 8)
+	 (fill-column . 80))))
